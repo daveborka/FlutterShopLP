23852a0bd912e1de3944dee1f7eb0c685b4390cd
daveborka <tregzor.dave@gmail.com>
2022-01-28 19:57:14 +0100

    Adding new product form elements

--- flutter_shop_app/lib/screens/edit_product_screen.dart
+++ flutter_shop_app/lib/screens/edit_product_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class EditProductScreen extends StatefulWidget {
   static String routeName = "/edit-product";
@@ -8,6 +7,34 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+  final _imageUrlTextController = TextEditingController();
+  final _imageFocusNode = FocusNode();
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _imageFocusNode.removeListener(updateImageUrl);
+    _descriptionFocusNode.dispose();
+    _priceFocusNode.dispose();
+    _imageUrlTextController.dispose();
+    _imageFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    _imageFocusNode.addListener(updateImageUrl);
+    // TODO: implement initState
+    super.initState();
+  }
+
+  void updateImageUrl() {
+    if (!_imageFocusNode.hasFocus) {
+      setState(() {});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +49,64 @@ class _EditProductScreenState extends State<EditProductScreen> {
             TextFormField(
               decoration: InputDecoration(labelText: 'Title'),
               textInputAction: TextInputAction.next,
+              onFieldSubmitted: (_) {
+                FocusScope.of(context).requestFocus(_priceFocusNode);
+              },
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Price'),
+              textInputAction: TextInputAction.next,
+              keyboardType: TextInputType.number,
+              focusNode: _priceFocusNode,
+              onFieldSubmitted: (_) {
+                FocusScope.of(context).requestFocus(_descriptionFocusNode);
+              },
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Description'),
+              maxLines: 3,
+              keyboardType: TextInputType.multiline,
+              focusNode: _descriptionFocusNode,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  child: _imageUrlTextController.text.isEmpty
+                      ? Text('Enterl a URL')
+                      : FittedBox(
+                          child: Image.network(_imageUrlTextController.text),
+                          fit: BoxFit.fill,
+                        ),
+                  width: 100,
+                  height: 100,
+                  margin: EdgeInsets.only(top: 8, right: 10),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Image url'),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      controller: _imageUrlTextController,
+                      onEditingComplete: () {
+                        setState(() {});
+                      },
+                      focusNode: _imageFocusNode,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         )),
